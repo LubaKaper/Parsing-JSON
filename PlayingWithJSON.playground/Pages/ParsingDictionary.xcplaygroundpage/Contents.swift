fcfb467ddcadcce5a00fa89c126f309acf02b493
LubaKaper <liubovkaper@pursuit.org>
2020-07-28 12:24:37 -0400

    successfully parsed contacts dictionary

--- PlayingWithJSON.playground/Pages/ParsingDictionary.xcplaygroundpage/Contents.swift
+++ PlayingWithJSON.playground/Pages/ParsingDictionary.xcplaygroundpage/Contents.swift
@@ -1,1 +1,48 @@
+import Foundation
+
+
 // PArsing Dictionary
+
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+//==============================================================================
+
+// Create Models
+
+//==============================================================================
+
+// Codable: Decodable and Encodable
+//Decodable: converts json to data
+// Encodable: converts to jsondata. Post to Web API
+
+struct ResultsWrapper: Decodable {
+    let results: [Contact]
+}
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+//==============================================================================
+// decode JSON data to our swift model
+//==============================================================================
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results// [Contact]
+    dump(contacts)
+} catch {
+   print("decoding error: \(error)")
+}
